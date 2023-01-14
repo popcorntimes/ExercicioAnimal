472c549968f33aa9701da4d22ab31caac9dee3d1
popcorntimes <sacercrisan@gmail.com>
2023-01-14 15:10:48 -0300

    Class Animal

--- ExercicioAnimal/Program.cs
+++ ExercicioAnimal/Program.cs
@@ -1,10 +1,61 @@
-﻿namespace ExercicioAnimal
+﻿using System;
+namespace ExercicioAnimal
 {
-    internal class Program
+    class Animal
     {
-        static void Main(string[] args)
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public char Sexo { get; set; }
+        public bool Carnivoro { get; set; }
+        public bool Peconhento { get; set; }
+        public int Idade()
         {
-            Console.WriteLine("Hello, World!");
+            var hoje = DateTime.Today;
+            var idade = hoje.Year - this.DataNascimento.Year;
+            return idade;
         }
+
+        public void Movimentar()
+        {
+             Console.WriteLine("Eu me remexo muito!");
+        }
+
+        public void Comunicar()
+        {
+            Console.WriteLine("Ich bin ein Tier");
+        }
+
+        public void Alimentar()
+        {
+            Console.WriteLine("Vou de comes e bebes");
+        }
+
+
     }
+    class Mamifero
+    {
+
+    }
+    
+    class Reptil
+    {
+
+    }
+
+    interface IAquatico
+    {
+
+    }
+
+    interface IOviparo
+    {
+
+    }
+
+    interface IVoar
+    {
+
+    }
+
+
 }
